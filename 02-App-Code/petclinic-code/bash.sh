2a0c6de6409d044d023d2ee168598496b69339bc
Vikash Kumar <vikashbhardwaj64@gmail.com>
2021-04-03 16:41:15 +0530

    Delete bash.sh

--- 02-App-Code/petclinic-code/bash.sh
+++
@@ -1,4 +0,0 @@
-- script: |
-    echo "##vso[task.setvariable variable=JAVA_HOME]$(JAVA_HOME_11_X64)"
-    echo "##vso[task.setvariable variable=PATH]$(JAVA_HOME_11_X64)/bin:$(PATH)"
-  displayName: "Set java version"
